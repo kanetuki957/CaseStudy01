9e28a24e3f398002d6cda41b3a1dd17e689f6ca3
muto4649 <muhyoto11815@gmail.com>
2025-04-30 14:57:10 +0900

    逆向きベルトコンベア制御
    スイッチで動かせるように実装

---
+++ Assets/GimmickFolder/GimmickAction/MoveTarget.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 指定のGameObjectを左に移動させる処理を管理するスクリプト
+public class MoveTarget : MonoBehaviour
+{
+    // 左方向への移動速度
+    public float moveSpeed = 2f;
+
+    // このX座標より左に来たら移動を停止する
+    public float stopXPosition = -10f;
+
+    // 移動中かどうかのフラグ
+    private bool isMoving = false;
+
+    void Update()
+    {
+        // 移動フラグが立っている間は左に移動し続ける
+        if (isMoving)
+        {
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+
+            // 指定座標より左に出たら止める
+            if (transform.position.x <= stopXPosition)
+            {
+                isMoving = false;
+            }
+        }
+    }
+
+    // 外部からこの関数を呼ぶことで、左向きにして移動を開始する
+    public void StartMovingLeft()
+    {
+        // 右向きでも必ず左向きにする（スケール反転）
+        Vector3 scale = transform.localScale;
+        scale.x = -Mathf.Abs(scale.x);
+        transform.localScale = scale;
+
+        // 移動開始
+        isMoving = true;
+    }
+}
---
+++ Assets/GimmickFolder/GimmickAction/SwitchController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// スイッチとなるGameObjectにアタッチし、クリック時に対象オブジェクトを動かす処理
+public class SwitchController : MonoBehaviour
+{
+    // 対象のオブジェクト
+    public GameObject targetObject;
+
+    // このGameObjectがクリックされたときに呼ばれる処理（マウス操作）
+    void OnMouseDown()
+    {
+        // 対象オブジェクトが設定されているか確認
+        if (targetObject != null)
+        {
+            // MoveTarget スクリプトを探す
+            var moveScript = targetObject.GetComponent<MoveTarget>();
+            if (moveScript != null)
+            {
+                // 対象オブジェクトが設定されているか確認
+                moveScript.StartMovingLeft();
+            }
+        }
+    }
+}
