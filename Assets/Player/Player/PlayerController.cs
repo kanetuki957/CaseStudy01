40222b823169abe273c4f771e1622e3f3f170001
ryo20031230 <kazunoko151230@gmail.com>
2025-05-19 20:22:38 +0900

    プレイヤーのアニメーション（ついてくる子熊）
    プレイヤーについてくる熊の実装
    プレイヤー（小熊も）アニメーションの切り替え
    停滞時、移動時、ゴール時の切り替え

--- Assets/Player/Player/PlayerController.cs
+++ Assets/Player/Player/PlayerController.cs
@@ -6,22 +6,18 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
 
-    public float moveSpeed = 5f;
-    public float jumpForce = 8f;
-    public Button startButton;
-    public Button restartButton; 
-    public Collider targetBlockCollider;
-
-
-    private bool Button = false;
-    private bool playerDirection = true;
-    private bool isGrounded;
-    private bool isWall;
-    private float playerWidth;
-    private Vector3 move;
-    private Rigidbody rb;
-    private SpriteRenderer spriteRenderer;
-    private float moveX;
+    public float moveSpeed = 5f;　//スピード
+    public Button startButton;　　//スタートボタンの判定
+    public Button restartButton; //リセットボタンの判定
+    public Collider targetBlockCollider;//ターゲットの当たり判定
+    private bool Button = false;　　　　//ボタン判定
+    private bool playerDirection = true;　//プレイヤーの向き
+    private bool isGrounded;              //地面の設置判定
+    private bool isWall;                  //壁の当たり判定
+    private Vector3 move;                 //move変数
+    private Rigidbody rb;                 
+    private SpriteRenderer spriteRenderer; 
+    private float moveX;                   //X軸のmove変数
 
     void Start()
     {
@@ -30,7 +26,7 @@ public class PlayerController : MonoBehaviour
         startButton.onClick.AddListener(MoveButton);
         restartButton.onClick.AddListener(RestartButton);
 
-        playerWidth = spriteRenderer.bounds.size.x;  // スプライトの幅を取得
+        //playerWidth = spriteRenderer.bounds.size.x;  // スプライトの幅を取得
         rb.freezeRotation = true; //
     }
         void Update()
@@ -39,8 +35,8 @@ public class PlayerController : MonoBehaviour
         {
             HandleMovement();
         }
-      
         HandleSpriteDirection();
+
     }
 
     void MoveButton()
@@ -57,25 +53,23 @@ public class PlayerController : MonoBehaviour
     // **移動処理**
     void HandleMovement()
     {
+        //プレイヤーの向きを取得
         Vector3 direction = playerDirection? Vector3.right : Vector3.left;
-        Vector3 rayOrigin = transform.position + Vector3.up * 1f; // 頭の位置から発射
+        Vector3 rayOrigintop = transform.position + Vector3.up * 1f; // 頭の位置から発射
+        Vector3 rayOrigindown = transform.position + Vector3.up * 1f; // 頭の位置から発射
         Vector3 rayOrigin1 = transform.position + Vector3.down * 0.5f; // 足元からレイを飛ばす
 
         // 前方に壁があるかチェック
-        if (Physics.Raycast(rayOrigin, direction, 0.5f))
+        if (Physics.Raycast(rayOrigintop, direction, 0.2f))
         {
           playerDirection = !playerDirection;  // 反転
         }
+        else if(Physics.Raycast(rayOrigindown, direction, 0.2f))
+        {
+            playerDirection = !playerDirection;  // 反転
+        }
 
         RaycastHit hit;
-        //if (Physics.Raycast(rayOrigin1, Vector3.down, out hit, 1f))
-        //{
-
-        //    float slopeAngle = Vector3.Angle(hit.normal, Vector3.up); // 地面の傾斜角を取得
-        //    //Debug.Log("傾斜角: " + slopeAngle);
-        //    //rb.velocity += Vector3.down * 2f * Time.deltaTime;  // 重力を強める
-
-        //}
         if (Physics.Raycast(rayOrigin1, Vector3.down, out hit, 10f))
         { // 最大10mの範囲で判定
             float groundDistance = Mathf.Round(hit.distance* 10.0f);
@@ -83,16 +77,7 @@ public class PlayerController : MonoBehaviour
             //Debug.Log("床までの距離: " + groundDistance);
             rb.velocity += Vector3.down * 2f * Time.deltaTime;
         }
-
-
-
-
-
-      
-        
-
-
-        //向きによって進む方向を変える
+       //向きによって進む方向を変える
        if (playerDirection)
        {
                moveX = +1.0f;
@@ -108,33 +93,11 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    // **ジャンプ処理**
-    //void HandleJump()
-    //{
-    //    //着地判定
-    //    isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.5f);
 
-
-    //    if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-    //    {
-    //        rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
-    //    }
-
-    //    if (!isGrounded && rb.velocity.y < 0)
-    //    {
-    //        rb.velocity += Vector3.up * Physics.gravity.y * 1.0f * Time.deltaTime;
-    //    }
-    //    if(!isGrounded && rb.velocity.y > 0)
-    //    {
-    //        rb.velocity += Vector3.up * Physics.gravity.y * 0.1f * Time.deltaTime;
-    //    }
-    //}
-
-    // **プレイヤーの向き**
     void HandleSpriteDirection()
     {
-      //無地の表示を変える
-      if (playerDirection)
+        //無地の表示を変える
+        if (playerDirection)
         {
             spriteRenderer.flipX = false; // 左向き
         }
@@ -143,13 +106,18 @@ public class PlayerController : MonoBehaviour
             spriteRenderer.flipX = true; // 右向き
         }
     }
- 
+
     void OnCollisionEnter(Collision collision)
     {
         if (collision.collider == targetBlockCollider)
         {
             Debug.Log("Touched specific block!");
             enabled = false;  // スクリプトの動作を停止
+        }
+        if(collision.gameObject.name == "Goal")
+        {
+            Button = false;
+
         }
     }
 }
--- Assets/Player/Player/RestartCharacter.cs
+++ Assets/Player/Player/RestartCharacter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RestartPlayer : MonoBehaviour
+public class RestartCharacter: MonoBehaviour
 {
     public Button startButton;
     private Vector3 startPosition;
