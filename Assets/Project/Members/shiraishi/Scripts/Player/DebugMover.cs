a40ac98cd03e78eec723d9bff72fc3685aad6af5
shira1-4108 <kakia4dkukeko108@gmail.com>
2025-06-13 15:28:18 +0900

    PlayerMove改造ver.2.0
    transformを直接変更して動かす仕様からrb.velocityを変更して動かす仕様に。それに伴ったコンベアのバグ修正

--- Assets/Project/Members/shiraishi/Scripts/Player/DebugMover.cs
+++ Assets/Project/Members/shiraishi/Scripts/Player/DebugMover.cs
@@ -14,6 +14,9 @@ public class DebugMover : MonoBehaviour
     void Update()
     {
         float move = Input.GetAxis("Horizontal");
+        if (move > 0.0) { move = 1.0f; } // ‰E•ûŒü
+        else if (move < 0.0) { move = -1.0f; } // ¶•ûŒü
+        else { move = 0.0f; } // ’âŽ~
         rb.velocity = new Vector2(move * speed, rb.velocity.y);
     }
 }
