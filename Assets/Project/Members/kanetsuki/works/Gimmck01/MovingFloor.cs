0ac342acd8ab56c6731d1a8f2c04e37091911925
kanetuki957 <hfdiqo4@gmail.com>
2025-06-13 17:56:59 +0900

    レバーに当たると動く床が動くようになる

--- Assets/Project/Members/kanetsuki/works/Gimmck01/MovingFloor.cs
+++ Assets/Project/Members/kanetsuki/works/Gimmck01/MovingFloor.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingFloor : MonoBehaviour
+public class MovingFloor : MonoBehaviour,IActivatable
 {
     [Header("移動の設定")]
     public float moveDistance = 2f;  // 上下に動く距離
     public float moveSpeed = 2f;     // 動くスピード
+
+    public bool isActive=false;
 
     private Vector3 startPos;
 
@@ -17,9 +19,20 @@ public class MovingFloor : MonoBehaviour
 
     void Update()
     {
+        if (isActive)
+        {
         // Sin波を使って上下に移動
         float newY = startPos.y + Mathf.Sin(Time.time * moveSpeed) * moveDistance;
         transform.position = new Vector3(startPos.x, newY, startPos.z);
+
+        }
     }
+    
+    public void Activate()
+    {
+        isActive = !isActive;
+
+    }
+
 
 }
