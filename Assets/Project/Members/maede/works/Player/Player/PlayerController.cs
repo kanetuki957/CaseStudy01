c958055b14738be24cbf94d24a0e2953c2f07284
ryo20031230 <kazunoko151230@gmail.com>
2025-06-27 17:28:24 +0900

    更新

--- Assets/Project/Members/maede/works/Player/Player/PlayerController.cs
+++ Assets/Project/Members/maede/works/Player/Player/PlayerController.cs
@@ -150,6 +150,7 @@ public class PlayerController : MonoBehaviour
 
                     if (!animationController.ladder )
                     {
+                        Debug.Log(hit.collider.name);
                         //Debug.Log("ê^ÇÒíÜ" + hitwalls.collider.name);
                         playerDirection = !playerDirection;  // îΩì]
                     }
